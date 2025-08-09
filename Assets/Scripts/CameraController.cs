b9b1500610ca28f7471c98a12df87c9c09643607
Ohno17 <ajaylego30@gmail.com>
2025-08-09 03:32:56 -0500

    initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField] private float sensitivity = 3;
+    [SerializeField] private Vector2 frameOffset = new(0, 1);
+    [SerializeField] private Transform target;
+    [SerializeField] private float followDistance = 5;
+
+    private float rotationX;
+    private float rotationY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationX += -Input.GetAxis("Mouse Y") * sensitivity;
+        rotationY += Input.GetAxis("Mouse X") * sensitivity;
+
+        rotationX = Mathf.Clamp(rotationX, -89, 89);
+
+        Quaternion targetRotation = Quaternion.Euler(rotationX, rotationY, 0);
+        Vector3 focusPosition = target.position + new Vector3(frameOffset.x, frameOffset.y);
+
+        Vector3 offsetVector = focusPosition - targetRotation * new Vector3(0, 0, followDistance);
+
+        transform.SetPositionAndRotation(offsetVector, targetRotation);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed = 6;
+    public float jumpStrength = 18;
+    public float velocityDampening = 0.8f;
+
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private Transform playerCamera;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundCheckMask;
+
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = new();
+        isGrounded = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity *= velocityDampening;
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.1f, groundCheckMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -1;
+        }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new(horizontal, 0, vertical);
+
+        if (direction.magnitude >= 0.1)
+        {
+            float moveAngle = playerCamera.eulerAngles.y + Mathf.Rad2Deg * (float)Math.Atan2(direction.x, direction.z);
+            Vector3 moveDirection = Quaternion.Euler(0f, moveAngle, 0f) * Vector3.forward;
+
+            velocity += moveSpeed * moveDirection.normalized;
+        }
+
+        velocity += Physics.gravity;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(-2 * Physics.gravity.y * jumpStrength);
+        }
+        
+        characterController.Move(velocity * Time.deltaTime);
+    }
+}
