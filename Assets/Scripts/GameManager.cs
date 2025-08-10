3565cc4640f352fc738fcb3d33a3d1fd2518fe3b
Ohno17 <ajaylego30@gmail.com>
2025-08-10 01:09:08 -0500

    parameterized

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,12 +6,13 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private string playerSceneName;
+    [SerializeField] private string firstLoadedSceneName;
 
     // Start is called before the first frame update
     void Start()
     {
         SceneManager.LoadScene(playerSceneName, LoadSceneMode.Additive);
-        SceneManager.LoadScene("TestLevel", LoadSceneMode.Additive);
+        SceneManager.LoadScene(firstLoadedSceneName, LoadSceneMode.Additive);
     }
 
     // Update is called once per frame
